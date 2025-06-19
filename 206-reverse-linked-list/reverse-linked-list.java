58961ad3c07dfa647522d3427ad31ba7070727f0
Vishhh2125 <167734203+Vishhh2125@users.noreply.github.com>
2025-06-19 23:41:45 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (67.04%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.java
+++ 206-reverse-linked-list/reverse-linked-list.java
@@ -9,54 +9,24 @@
  * }
  */
 class Solution {
-
-    public static  ListNode getnode(int index,ListNode head){
-        int currentindex=-1;
-        ListNode temp=head;
-
-        while(temp!=null){
-            currentindex++;
-
-            if(currentindex==index)  break ;
-
-              temp=temp.next;
-
-        }
-
-        return temp ;
-
-    }
     public ListNode reverseList(ListNode head) {
 
-        ListNode current=head;
-        int size=0;
+        if(head==null)  return null;
 
-        while(current!=null){
-            size++;
-            current=current.next;
-        }
-
-      
-
+        ListNode pre=null;
         
-
-        for(int i =0;i<size/2;i++){
-              ListNode leftnode=getnode(i,head);
-             ListNode rightnode=getnode(size-1-i,head);
-
-             int temp = leftnode.val;
-             leftnode.val=rightnode.val;
-             rightnode.val=temp;
-
+         ListNode current=head;
+        while(current!=null){
+            ListNode post=current.next;  // save next node refernce 
+            current.next=pre;         //pont current to the pre 
+            pre=current;                 // give pre to current
+            current=post;                   //set current to the next node 
+             
+        
         }
+        head=pre;  //it will come to tail pointer
 
         return head;
-
-
-
-        
-
-         
         
     }
 }
