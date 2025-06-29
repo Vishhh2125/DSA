128b969d07f8007a99bf95512618d9e21788487a
Vishhh2125 <167734203+Vishhh2125@users.noreply.github.com>
2025-06-29 23:19:19 +0530

    Time: 68 ms (63.87%) | Memory: 57.7 MB (44.06%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -1,73 +1,22 @@
 class Solution {
-    public int largestRectangleArea(int[] arr) {
-      int n= arr.length;
-    int [] smallest_right=new int [n];
+    public int largestRectangleArea(int[] heights) {
+        Stack<Integer> st = new Stack<>();
+        int maxArea = 0;
+        int n = heights.length;
 
-    int [] smallest_left=new int [n];
-    Stack<int []> st = new Stack<>();
+        for (int i = 0; i <= n; i++) {
+            int currHeight = (i == n) ? 0 : heights[i];  // Sentinel 0 at end
 
-    for(int i=0;i<n;i++){
+            while (!st.isEmpty() && currHeight < heights[st.peek()]) {
+                int height = heights[st.pop()];
+                int width = st.isEmpty() ? i : i - st.peek() - 1;
+                int area = height * width;
+                maxArea = Math.max(maxArea, area);
+            }
 
-        while(!st.isEmpty() && arr[i]<=st.peek()[0]){
-            st.pop();
+            st.push(i);
         }
 
-        if(st.isEmpty()){
-            //current o sthe smallest
-            smallest_left[i]=-1;
-        }else{
-            //peek is smalest 
-
-            smallest_left[i]= st.peek()[1];
-        }
-
-        st.push(new int[]{arr[i],i});
-    }
- 
-        while(!st.isEmpty()){
-            st.pop();
-            //empty stack for further uses 
-        }
-
-
-
-
-        for(int i=n-1;i>=0;i--){
-
-        while(!st.isEmpty() && arr[i]<=st.peek()[0]){
-            st.pop();
-        }
-
-        if(st.isEmpty()){
-            //current o sthe smallest
-            smallest_right[i]=n;
-        }else{
-            //peek is smalest 
-
-            smallest_right[i]= st.peek()[1];
-        }
-
-        st.push(new int[]{arr[i],i});
-    }
-
-
-    int max=0;
-
-
-    for(int i=0;i<n;i++){
-        int right=smallest_right[i];
-        int left=smallest_left[i];
-
-        int ans=(right-left-1)* arr[i];
-        
-
-        max=Math.max(ans,max);
-    }
-
-    return max;
-
-
-
-        
+        return maxArea;
     }
 }
