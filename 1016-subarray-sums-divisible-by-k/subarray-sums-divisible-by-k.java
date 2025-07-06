15a51407ced7a07660fde6ca23f61db4bf479395
Vishhh2125 <167734203+Vishhh2125@users.noreply.github.com>
2025-07-06 22:09:20 +0530

    Time: 23 ms (65.49%) | Memory: 49.8 MB (11.22%) - LeetSync

--- 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.java
+++ 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.java
@@ -1,6 +1,11 @@
 class Solution {
     public int subarraysDivByK(int[] arr, int k) {
 
+
+        // (arr[j]-arr[i])%k==0
+
+        // arr[i]%k=arr[j]%k   ----->this is the conditin if any ith index mod i same to pevious ten it is the one subarry with valid sum condition 
+
         int n =arr.length;
 
     Map<Integer, Integer> map = new HashMap<>();
@@ -13,15 +18,16 @@ class Solution {
         for(int i =0;i<n;i++){
             sum+=arr[i];
 
-            int mod=sum%k;
+            // int mod=sum%k;
+             int mod = (sum % k + k) % k;
 
-            if(mod<0){
-                mod=mod+k;
+            // if(mod<0){
+            //     mod=mod+k;
 
             //    If you're at -2 hours on a 5-hour clock...
             // That's the same as being at 3 hours.
 
-            }
+            // }
 
             count+=map.getOrDefault(mod,0);  
 
