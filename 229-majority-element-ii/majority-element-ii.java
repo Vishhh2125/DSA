cad6f4e8ae69305fcc241aa0faa3e7f53cd89f7d
Vishhh2125 <167734203+Vishhh2125@users.noreply.github.com>
2025-07-13 15:59:02 +0530

    Time: 4 ms (60.40%) | Memory: 50.9 MB (5.04%) - LeetSync

---
+++ 229-majority-element-ii/majority-element-ii.java
@@ -0,0 +1,36 @@
+class Solution {
+    public List<Integer> majorityElement(int[] arr) {
+
+        Integer major1 = null, major2 = null;
+        int count1 = 0, count2 = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (major1 != null && arr[i] == major1) {
+                count1++;
+            } else if (major2 != null && arr[i] == major2) {
+                count2++;
+            } else if (count1 == 0) {
+                major1 = arr[i];
+                count1 = 1;
+            } else if (count2 == 0) {
+                major2 = arr[i];
+                count2 = 1;
+            } else {
+                count1--;
+                count2--;
+            }
+        }
+
+        int freq1 = 0, freq2 = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == major1) freq1++;
+            else if (arr[i] == major2) freq2++;
+        }
+
+        List<Integer> ans = new ArrayList<>();
+        if (freq1 > arr.length / 3) ans.add(major1);
+        if (freq2 > arr.length / 3 && !major2.equals(major1)) ans.add(major2);
+
+        return ans;
+    }
+}
