8e454957e87397e43f64c3a18a26e2d9f71b3f40
Vishhh2125 <167734203+Vishhh2125@users.noreply.github.com>
2025-08-18 12:34:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sort a stack/sort-a-stack.java
+++ Difficulty: Medium/Sort a stack/sort-a-stack.java
@@ -1,33 +1,39 @@
 /*Complete the function below*/
 class GfG {
+    
+    public static  Stack<Integer> function(Stack<Integer> s ,Stack<Integer> ans){
+        
+        
+        if(s.isEmpty())  return ans;
+        
+        
+        int top =s.peek();
+        s.pop();
+        
+        
+        if(ans.isEmpty()){
+            ans.push(top);
+        }else{
+            
+            while(!ans.isEmpty() && top<ans.peek()){
+                
+                s.push(ans.peek());
+                ans.pop();
+                
+                
+                
+            }
+            
+            ans.push(top);
+        }
+        
+        return function(s,ans);
+    }
     public Stack<Integer> sort(Stack<Integer> s) {
         // add code here.
         
-        Stack<Integer>  ans = new Stack<>();    
         
-        
-        while(!s.isEmpty()){
-            
-            
-            int top= s.peek();
-            s.pop();
-            
-            
-            if(ans.isEmpty()){
-                ans.push(top);
-            }else{
-                
-                
-                while(!ans.isEmpty() && top<ans.peek()){
-                    
-                    s.push(ans.peek());
-                    ans.pop();
-                }
-                
-                ans.push(top);
-            }
-        }
-        
-        return ans;
+        Stack<Integer> ans= new Stack<>();
+        return function(s,ans);
     }
 }
