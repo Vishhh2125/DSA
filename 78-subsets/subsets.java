b9fa2bfa093ea5afe511275a9be2bf38df0d9518
Vishnu Uplenchwar <167734203+Vishhh2125@users.noreply.github.com>
2025-08-26 00:01:59 +0530

    Time: 1 ms (48.77%) | Memory: 42.9 MB (46.28%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,30 +1,36 @@
 class Solution {
-    public List<List<Integer>> subsets(int[] arr) {
-         List<List<Integer>> ans = new ArrayList <>();
-      int bits = arr.length;
 
-      int totalCombination= 1<<bits;
+    public static void solution(List<Integer> input ,List<Integer>  output,List<List<Integer>> ans){
 
+        if(input.size()==0){
 
-      for(int i=0;i<totalCombination;i++){
-         List<Integer> set = new ArrayList<>();
-
-        for(int j =0;j<bits;j++){
-            int mask =1<<j;
-
-            if((i & mask) !=0){
-
-                set.add(arr[j]);
-
-            }
-
+            ans.add(output);
+            return ;
         }
-                    ans.add(set);
 
-      }
+        int num=input.get(0); //extract first element 
+            input.remove(0);//remove first element 
 
+        ArrayList<Integer>  output1=new ArrayList<>(output);
+        output1.add(num); 
 
-      return ans;
+         ArrayList<Integer>  output2=new ArrayList<>(output);
 
+        solution(new ArrayList<>(input),output1,ans);//added num for combination
+        solution(input,output2,ans);//not added num
+        
+    }
+    public List<List<Integer>> subsets(int[] arr) {
+
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> input=new ArrayList<>();
+          for(int num:arr){
+            input.add(num);
+
+          }
+        solution(input,new ArrayList<>(),ans);
+
+        return ans;
+        
     }
 }
