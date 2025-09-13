185b7f357229f784bccd7a04a4171d6d6120bee8
Vishnu Uplenchwar <167734203+Vishhh2125@users.noreply.github.com>
2025-09-13 11:31:35 +0530

    Time: 41 ms (9.19%) | Memory: 45.3 MB (11.36%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
@@ -1,25 +1,43 @@
 class Solution {
     public int numberOfSubstrings(String s) {
-        int[] count = new int[3]; // counts of 'a','b','c'
-        int left = 0, result = 0, unique = 0;
 
-        for (int right = 0; right < s.length(); right++) {
-            int rIndex = s.charAt(right) - 'a';
-            count[rIndex]++;
-            if (count[rIndex] == 1) unique++;
 
-            while (unique == 3) {
-                // For each valid window [left, right], 
-                // all substrings starting at any index <= left and ending at right are valid
-                result += s.length() - right;
+        HashMap<Character,Integer> map= new HashMap<>();
 
-                int lIndex = s.charAt(left) - 'a';
-                count[lIndex]--;
-                if (count[lIndex] == 0) unique--;
-                left++;
-            }
+        int left =0;
+        int result=0;
+
+        for(int right =0;right<s.length();right++){
+
+            map.put(s.charAt(right),map.getOrDefault(s.charAt(right),0)+1);
+
+
+          
+
+                while(map.size()==3){
+
+                    result+=s.length()-right;//getting all combination with this substrring which is valid from right to end 
+                    
+                    map.put(s.charAt(left),map.get(s.charAt(left))-1);//remove from left as freq
+
+                     if(map.get(s.charAt(left))==0){
+                             //its frequenchy is zero rremove the key from  hashamap
+
+                          map.remove(s.charAt(left));
+
+                        }
+
+                      left++;
+                    
+                 }
+            
         }
 
+
         return result;
+
+
+    
+        
     }
 }
