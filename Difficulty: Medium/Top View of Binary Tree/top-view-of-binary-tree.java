68a52a58b8b5fb866382d56d577091f8d9554d9a
Vishnu Uplenchwar <167734203+Vishhh2125@users.noreply.github.com>
2025-10-04 15:35:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Top View of Binary Tree/top-view-of-binary-tree.java
@@ -0,0 +1,73 @@
+/*
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int val) {
+        this.data = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+*/
+
+class Solution {
+    
+    public static class  Pair {
+        
+        Node  node;
+        int col;
+        
+        Pair(Node  node,int col){
+            this.node=node;
+            this.col=col;
+        }
+        
+        
+    }
+    public ArrayList<Integer> topView(Node root) {
+        // code here
+        
+        ArrayList<Integer> ans = new ArrayList<>();
+        Deque <Pair>  queue = new ArrayDeque<>();
+        
+        TreeMap<Integer,Integer>  map = new TreeMap<>();
+        
+        queue.add( new Pair(root,0));
+        while(!queue.isEmpty()){
+            
+            
+            int size= queue.size();
+            
+            for(int i =0;i<size;i++){
+                
+              Pair current = queue.poll();
+              
+              //operation on current node 
+              
+              if(!map.containsKey(current.col)){
+                  
+                  map.put(current.col,current.node.data);
+              }
+              
+              if(current.node.left!=null)  queue.add( new Pair(current.node.left,current.col-1));
+              
+              if(current.node.right!=null)  queue.add( new Pair(current.node.right,current.col+1));
+              
+              
+            }
+        }
+        
+        
+        
+        for(Integer num :map.values()){
+            
+            
+            ans.add(num);
+            
+            
+        }
+        
+        return ans ;
+    }
+}
