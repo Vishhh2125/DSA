19f49c1bc06f3a46dfce177d33c61c86627329f4
Vishnu Uplenchwar <167734203+Vishhh2125@users.noreply.github.com>
2025-10-17 18:07:18 +0530

    Time: 4 ms (18.39%) | Memory: 45.2 MB (52.17%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
@@ -14,43 +14,43 @@
  * }
  */
 class Solution {
-
-    public boolean find_pair( TreeNode root,int k ,int num1 ){
-
-        boolean ans= false;
-        TreeNode current =root;
-        while(current!=null){
-            if( current.val!=num1 && current.val+num1==k ){
-                ans=true;
-                break;
-            }
-            else if (k-num1<current.val) current=current.left;
-            else current=current.right;
-        }
-
-        return ans ;
-
-
-    
-    }
-
-    public boolean solution(TreeNode root,TreeNode current,int k ){
-        if(current==null)  return false;
-
-        boolean left = solution(root,current.left,k);
-
-        boolean curr= find_pair(root,k,current.val);
-
-        boolean right=solution(root,current.right,k);
-
-        return left || curr || right ;
-
-    }
     public boolean findTarget(TreeNode root, int k) {
 
-        return solution(root,root,k);
+        ArrayList<Integer> arr = new ArrayList<>();
+       Stack<TreeNode> st = new Stack<>();
 
+        TreeNode current=root;
+        while( current!=null || !st.isEmpty()){
+
+            while(current!=null){
+                st.push(current);
+                current=current.left;    //left
+
+            }
+
+            TreeNode node = st.pop();
+
+            arr.add(node.val);   //root
+
+            current=node.right;   //right 
+        }
         
-        
+
+    int i=0;
+    int j=arr.size()-1;
+
+    while(i<j){
+        if(arr.get(i)+arr.get(j)==k)  return true;
+        else if(arr.get(i)+arr.get(j)<k){
+            //need bigger sum 
+            //increase i 
+            i++;
+        }else{
+            j--;
+        }
+
+    }
+
+    return false;
     }
 }
