26daa932929647cad263c3ef9e804a4317969a1a
Vishnu Uplenchwar <167734203+Vishhh2125@users.noreply.github.com>
2025-10-18 21:30:40 +0530

    Time: 0 ms (100.00%) | Memory: 45.6 MB (32.89%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,15 +1,15 @@
 class Solution {
-    public int missingNumber(int[] arr) {
-        
-         int  sum=0;
-        for(int i=0;i<arr.length;i++){
-              
-           sum+=arr[i];
+    public int missingNumber(int[] nums) {
+         int sum =0;
+         int n =nums.length;
+        for(int i =0;i<n;i++){
+            sum+=nums[i];
+
+
 
         }
-         int n =arr.length;
-        int ideal= n * (n+1)/2;
 
+        int ideal =  (n *(n+1))/2;
 
         return ideal-sum;
         
