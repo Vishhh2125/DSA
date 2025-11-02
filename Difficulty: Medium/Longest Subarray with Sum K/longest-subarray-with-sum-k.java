6b9944b5609cd385a66c270220f06e0b2a9c3f07
Vishnu Uplenchwar <167734203+Vishhh2125@users.noreply.github.com>
2025-11-02 12:07:26 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Subarray with Sum K/longest-subarray-with-sum-k.java
+++ Difficulty: Medium/Longest Subarray with Sum K/longest-subarray-with-sum-k.java
@@ -3,29 +3,35 @@
 class Solution {
     public int longestSubarray(int[] arr, int k) {
         // code here
+        
+        HashMap<Integer,Integer> map = new HashMap<>();
+        
+        map.put(0,-1);
+        
+        int sum =0;
         int max=0;
-        int sum=0;
         
-        Map <Integer,Integer> map=new HashMap<>();
-        
-        map.put(0,-1); //here instead of giveing count of sum we are giveing the position of first sum
         
         for(int i=0;i<arr.length;i++){
+            
             sum+=arr[i];
-            //condition checking 
+            
             if(map.containsKey(sum-k)){
-                int initialIndex=map.get(sum-k);
-                
-            max=  Math.max(max,i-initialIndex);
+                int len = i-map.get(sum-k);
+                max=Math.max(max,len);
             }
             
             if(!map.containsKey(sum)){
                 map.put(sum,i);
             }
-            
+              
+              
+              
             
         }
         
+        
         return max;
+        
     }
 }
