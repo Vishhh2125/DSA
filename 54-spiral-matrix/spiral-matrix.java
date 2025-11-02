0c57d56b2eef15b447b8f140300da78c6e1fc9b1
Vishnu Uplenchwar <167734203+Vishhh2125@users.noreply.github.com>
2025-11-02 19:50:27 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (8.61%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.java
+++ 54-spiral-matrix/spiral-matrix.java
@@ -1,43 +1,74 @@
 class Solution {
-    public List<Integer> spiralOrder(int[][] matrix) {
-        List<Integer> result = new ArrayList<>();
+    public List<Integer> spiralOrder(int[][] arr) {
+        
+        int minc=0;
+        int maxc=arr[0].length-1;
+        int minr=0;
+        int maxr=arr.length-1;
+        ArrayList<Integer> ans = new ArrayList<>();
+ 
+        while(minc<=maxc && minr <=maxr){
 
-        int top = 0;
-        int bottom = matrix.length - 1;
-        int left = 0;
-        int right = matrix[0].length - 1;
 
-        while (top <= bottom && left <= right) {
+        //from left tto right 
+         //i=minr
+         //j=minc to maxc
 
-            // left to right
-            for (int i = left; i <= right; i++) {
-                result.add(matrix[top][i]);
-            }
-            top++;
 
-            // top to bottom
-            for (int i = top; i <= bottom; i++) {
-                result.add(matrix[i][right]);
-            }
-            right--;
+         for(int j=minc ;j<=maxc;j++){
+            ans.add(arr[minr][j]);
 
-            // right to left
-            if (top <= bottom) {
-                for (int i = right; i >= left; i--) {
-                    result.add(matrix[bottom][i]);
-                }
-                bottom--;
-            }
 
-            // bottom to top
-            if (left <= right) {
-                for (int i = bottom; i >= top; i--) {
-                    result.add(matrix[i][left]);
-                }
-                left++;
-            }
+         }
+            minr++;
+
+         //from top to bottom 
+         // i=minr to maxr 
+         // j=maxc
+
+         for(int i=minr ;i<=maxr;i++){
+            ans.add(arr[i][maxc]);
+         }
+         maxc--;
+
+
+         //from right to left 
+         //i=maxr
+         //j=maxc to minc
+
+          if (minr <= maxr) {
+
+         for(int j=maxc;j>=minc;j--){
+
+            ans.add(arr[maxr][j]);
+         }
+
+         maxr--;
+
+          }
+
+
+         //from bottom to top 
+         //i= maxr to min r 
+         //j=minc 
+
+         if(minc<=maxc){
+
+         for(int i =maxr ;i>=minr;i--){
+            ans.add(arr[i][minc]);
+         }
+
+         minc++;
+         }
+
+
+
         }
 
-        return result;
+
+        return ans ;
+
+
+        
     }
 }
