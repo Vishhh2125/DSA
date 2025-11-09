05f7a0fed7fc992ac5178a34145f88d5b11d4bcf
Vishnu Uplenchwar <167734203+Vishhh2125@users.noreply.github.com>
2025-11-09 11:35:30 +0530

    Time: 4 ms (70.44%) | Memory: 46.9 MB (5.09%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,48 +1,34 @@
 class Solution {
     public int romanToInt(String s) {
 
-        Map<Character,Integer>  Map = new HashMap<>();
 
+        HashMap<Character,Integer>  map = new HashMap<>();
 
-        Map.put('I',1);
-        Map.put('V',5);
-        Map.put('X',10);
-        Map.put('L',50);
-         Map.put('C',100);
-        Map.put('D',500);
-        Map.put('M',1000);
+        map.put('I',1);
+        map.put('V',5);
+        map.put('X',10);
+        map.put('L',50);
+        map.put('C',100);
+        map.put('D',500);
+        map.put('M',1000);
+         int ans=0;
 
-           int ans=0;
-           
+        for(int i=0;i<s.length();i++){
 
-       for(int i=0;i<s.length();i++){
+            int  current= map.get(s.charAt(i));
 
-        int curr=Map.get(s.charAt(i));
+            if(i+1<s.length() && map.get(s.charAt(i+1))>current){
+               int  next = map.get(s.charAt(i+1));
+                ans+= next-current;
+                i++;
 
-         int next=0;
-                if(i+1<s.length()){
-                    next=Map.get(s.charAt(i+1));
-                    
-                }
-
-        if(curr<next){
-            ans-=curr;
-        }else{
-            ans+=curr;
+            }else{
+                //simply add
+                ans+=current;
+            }
         }
-
-       }
-
-
-        return ans;
-
-
-
-
-
-
-
         
+     return ans;    
         
     }
 }
