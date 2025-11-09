31f258cafbad9a08b46cd83cb61ef48cdcbfa75c
Vishnu Uplenchwar <167734203+Vishhh2125@users.noreply.github.com>
2025-11-09 09:15:20 +0530

    Time: 12 ms (56.05%) | Memory: 43.7 MB (10.18%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -1,43 +1,37 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
 
+        boolean ans= true;
 
-        Map<Character,Character>  map1 = new HashMap<>();
-        Map<Character,Character>  map2 = new HashMap<>();
-         boolean flag=true;
+        HashMap<Character,Character> map= new HashMap<>();
+        Set<Character> set = new HashSet<>();
+        for(int i=0;i<s.length();i++){
+            
+            if(map.containsKey(s.charAt(i))){
 
-        for(int i =0;i<s.length();i++){
+                char ch = map.get(s.charAt(i));
 
-            if(!map1.containsKey(s.charAt(i))){
 
-                map1.put(s.charAt(i),t.charAt(i));
-
-            }else{
-                char ch=map1.get(s.charAt(i));
                 if(ch!=t.charAt(i)){
-                
-                    flag=false;
-                    break;
-
+                   ans= false;
+                   break;
                 }
-            }
-
-
-            if(!map2.containsKey(t.charAt(i))){
-
-                map2.put(t.charAt(i),s.charAt(i));
-                
             }else{
-                char ch = map2.get(t.charAt(i));
-                if(ch!=s.charAt(i)){
-                    flag=false;
+                //mapping not present 
+                //add new  mapping 
+                if(set.contains(t.charAt(i))){
+                    ans=false;
                     break;
                 }
+                map.put(s.charAt(i),t.charAt(i));
+                set.add(t.charAt(i));
+            
             }
-
+        
         }
 
-     return flag;
+
+        return ans;
         
     }
 }
