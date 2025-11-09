c382a4e21595fc3dacc892658a1eeccee1d35bd3
Vishnu Uplenchwar <167734203+Vishhh2125@users.noreply.github.com>
2025-11-09 09:20:42 +0530

    Time: 12 ms (56.05%) | Memory: 43.9 MB (6.68%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -3,8 +3,8 @@ class Solution {
 
         boolean ans= true;
 
-        HashMap<Character,Character> map= new HashMap<>();
-        Set<Character> set = new HashSet<>();
+        HashMap<Character,Character> map= new HashMap<>(); // to map from s to t
+        Set<Character> set = new HashSet<>();  // to set weather the t character is ampped to naoythe ror not before mapping ftom s to t 
         for(int i=0;i<s.length();i++){
             
             if(map.containsKey(s.charAt(i))){
@@ -20,6 +20,8 @@ class Solution {
                 //mapping not present 
                 //add new  mapping 
                 if(set.contains(t.charAt(i))){
+                    //cheack before mapping the s to t werather t is mapped to any other 
+                    // beacuse map tracks s string char not t 
                     ans=false;
                     break;
                 }
