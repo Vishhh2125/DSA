814d3d689362aba0f9d46c70d9d72c835581a418
Vishnu Uplenchwar <167734203+Vishhh2125@users.noreply.github.com>
2025-12-05 22:56:32 +0530

    Time: 2 ms (9.16%) | Memory: 46.6 MB (6.25%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.java
+++ 2-add-two-numbers/add-two-numbers.java
@@ -10,91 +10,55 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        
 
-           int carry=0;
-         
-           ListNode temp1=l1;
-           ListNode temp2=l2;
-            ListNode ans= new ListNode();
-            ListNode current=ans;
+       ListNode  temp1=l1;
+        ListNode temp2=l2;
+        int carry=0;
+         ListNode ans = new ListNode(0);
+         ListNode current=ans;
+        while(temp1!=null || temp2!=null){
 
-           while(temp1!=null && temp2!=null){
-
-             int sum=temp1.val+temp2.val;
-
-             if(carry>0){
-                sum+=1;
-                carry=0;
-             }
-
-             if(sum>9){
-                sum=sum%10; //extract last dogit 
-                carry=1;
-             }
-             current.next= new ListNode(sum);
-             current=current.next;
-
-             temp1=temp1.next;
-             temp2=temp2.next;
+            int val1=temp1!=null ? temp1.val :0;
+            int val2=temp2!=null? temp2.val :0;
 
 
-
-           }
-
-
-         
-
-            while(temp1!=null){
-
-                int sum =temp1.val;
-
-                 if(carry>0){
-                   sum+=1;
-                  carry=0;
-                }
-
-                if(sum>9){
-                sum=sum%10; //extract last dogit 
-                carry=1;
-             }
-
-            current.next= new ListNode(sum);
-            current=current.next;
-            temp1=temp1.next;
-
-
-
-            }
-
-            while(temp2!=null){
-
-                 int sum =temp2.val;
-
-                if(carry>0){
-                   sum+=1;
-                  carry=0;
-                }
-
-                if(sum>9){
-                        sum=sum%10; //extract last dogit 
-                        carry=1;
-                 }
-
-                    current.next= new ListNode(sum);
-                    current=current.next;
-                    temp2=temp2.next;
-            
-
-
-            }
+            int sum =val1+val2;
 
 
             if(carry>0){
-                current.next=new ListNode(1);
+                sum=sum+1;
+                carry=0;
             }
 
-            return ans.next;
-           
+            if(sum>9){
+                sum=sum%10;
+                carry=1;
+            }
+
+
+            current.next= new ListNode(sum);
+            current=current.next;
+            if(temp1!=null){
+            temp1=temp1.next;
+
+            }
+            if(temp2!=null){
+
+            temp2=temp2.next;
+            }
+
+
+
+
+
+        }
+
+
+        if(carry>0){
+            current.next= new ListNode(1);
+        }
+
+        return ans.next;
+        
     }
 }
