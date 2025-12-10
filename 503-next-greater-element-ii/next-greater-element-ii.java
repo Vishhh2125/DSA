89a1b36ce2daaf9b8201c4efe5beb8733e865882
Vishnu Uplenchwar <167734203+Vishhh2125@users.noreply.github.com>
2025-12-10 22:53:23 +0530

    Time: 5 ms (95.04%) | Memory: 48.2 MB (46.62%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.java
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -1,29 +1,47 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
-        //same as next grestest elemnt just we have to check for two times thet if from 2n-1 to 0 
-        
-        int n =nums.length;
-        int [] ans =new int[n];
-         Stack<Integer> st= new Stack<>();
-        for(int i=2*n-1;i>=0;i--){
-            int index= i%n;
-            while(!st.isEmpty() && st.peek()<=nums[index] ){
-                st.pop();
-            }
 
-            
+         int n =nums.length;
+        int [] ans = new int [n];
+        
+
+
+        Deque<Integer> st = new ArrayDeque<>();
+
+        for(int i= (2*n)-1;i>=0;i--){
+
+            int num = nums[i%n];
+
+
+            while(!st.isEmpty() && num>=nums[st.peek()]){
+
+
+                st.pop();
+
+                
+
+
+            }
 
             if(st.isEmpty()){
-                ans[index]=-1;
+
+                ans[i%n]=-1;
             }else{
-                ans[index]= st.peek();
+
+                ans[i%n]=nums[st.peek()];
             }
 
-            st.push(nums[index]);
+
+            st.push(i%n);
+
+
+
         }
 
 
-        return ans;
+        return ans ;
+
+
         
     }
 }
