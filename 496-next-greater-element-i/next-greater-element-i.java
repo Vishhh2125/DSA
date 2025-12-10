8b795fa550bd0bd72ed40d8e8e619ccce5fbc43f
Vishnu Uplenchwar <167734203+Vishhh2125@users.noreply.github.com>
2025-12-10 22:29:14 +0530

    Time: 4 ms (58.32%) | Memory: 45.7 MB (5.04%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -2,39 +2,47 @@ class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
 
 
-        int n1=nums1.length;
-        int n2=nums2.length;
+        HashMap<Integer,Integer> map = new HashMap<>();
+         
 
+         Deque<Integer> st = new ArrayDeque<>();
 
-    Map<Integer, Integer> map = new HashMap<>();
-        Stack<Integer>  st = new Stack<>();
+        for(int i=nums2.length-1;i>=0;i--){
 
-        for(int i =n2-1;i>=0;i--){
+            int num =nums2[i];
 
-            while(!st.isEmpty() && st.peek()<=nums2[i]){
+              //here index= stack elemnt 
+            while(!st.isEmpty() && num>nums2[st.peek()]){    //comapring from nums2[index] 
                 st.pop();
             }
 
-            if(!st.isEmpty() && st.peek()>nums2[i]){
 
-                map.put(nums2[i],st.peek());
-            }else if(st.isEmpty()){
+            if(st.isEmpty()){
                 map.put(nums2[i],-1);
+            }else{
 
+                map.put(nums2[i],nums2[st.peek()]); 
             }
 
-            st.push(nums2[i]);
-
+            st.push(i); //always put current index 
 
         }
 
-         int [] ans= new int[n1];
 
-         for(int i=0;i<n1;i++){
 
-            ans[i]=map.get(nums1[i]);
-         }
+        int [] ans = new int[nums1.length];
+         int i=0;
 
-         return ans;
+        for(int num :nums1){
+
+            int val= map.get(num);
+
+            ans[i]=val;
+            i++;
+        }
+
+
+        return ans ;
+        
     }
 }
