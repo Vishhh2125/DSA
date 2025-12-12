104b77e56df00f02b050e7602fca16807a22e609
Vishnu Uplenchwar <167734203+Vishhh2125@users.noreply.github.com>
2025-12-13 00:48:35 +0530

    Time: 26 ms (86.51%) | Memory: 78.4 MB (25.10%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -1,26 +1,72 @@
 class Solution {
     public int largestRectangleArea(int[] arr) {
-        int n = arr.length;
-        int max = 0;
 
-        Deque<Integer> st = new ArrayDeque<>();
+        int [] left_smallest= new int [arr.length];
+        int [] right_smallest= new int  [arr.length];
 
-        for (int i = 0; i <= n; i++) {
-            int current = (i == n) ? 0 : arr[i];
+          Deque<Integer> st = new ArrayDeque<>();
+           //for first index -1 index is considered 0 and is smallest 
+        for(int i=0;i<arr.length;i++){
 
-            while (!st.isEmpty() && current < arr[st.peek()]) {
-                int top = st.pop();       // height index
-                int height = arr[top];
 
-                // width depends on stack after popping
-                int width = st.isEmpty() ? i : (i - st.peek() - 1);
+            while(!st.isEmpty() && arr[i]<=arr[st.peek()]){
 
-                max = Math.max(max, height * width);
+                st.pop();
             }
 
+
+            left_smallest[i]= st.isEmpty() ? -1 : st.peek();
+
             st.push(i);
+
+
+
         }
 
-        return max;
+
+        st.clear();
+
+
+        for(int i=arr.length-1;i>=0;i--){
+
+
+            while(!st.isEmpty() && arr[i]<=arr[st.peek()]){
+
+                st.pop();
+            }
+
+            right_smallest[i]= st.isEmpty() ? arr.length: st.peek();
+
+            st.push(i);
+
+
+
+        }
+
+
+        //now we have nsl and nsr
+
+         int max=0;
+        for(int i=0;i<arr.length;i++){
+
+
+            int width = right_smallest[i]-left_smallest[i]-1;
+
+            int height =arr[i];
+
+            int area= width * height ;
+
+
+             max= Math.max(area,max);
+
+
+        }
+
+
+
+        return  max;
+
+
+        
     }
 }
