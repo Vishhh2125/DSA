47d86b97aeba897b54b51c05613bbecb25421460
Vishnu Uplenchwar <167734203+Vishhh2125@users.noreply.github.com>
2025-12-16 16:49:33 +0530

    Time: 33 ms (37.57%) | Memory: 161.7 MB (5.01%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -2,10 +2,10 @@ class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
 
 
-       PriorityQueue<int[]> pq = new PriorityQueue<>(
-            (a, b) -> Integer.compare(b[0], a[0]) // max-heap by value
-        );        
-
+    //    PriorityQueue<int[]> pq = new PriorityQueue<>(
+    //         (a, b) -> Integer.compare(b[0], a[0]) // max-heap by value
+    //     );        
+        Deque<int []> st = new ArrayDeque<>();
         ArrayList<Integer> ans = new ArrayList<>();
 
 
@@ -14,31 +14,27 @@ class Solution {
 
         for(int j=0;j<nums.length;j++){
 
-             pq.add(new int[]{nums[j], j});
+             
 
+             while(!st.isEmpty() && nums[j]>st.peek()[0]){
+                st.pop();
+             }
 
+             st.push(new int[]{nums[j], j});
              if(j-i+1==k){
                 //window size reaced k 
 
-                 while(!pq.isEmpty() && pq.peek()[1]<i){
+                 while(!st.isEmpty() && st.peekLast()[1]<i){
 
-                    pq.poll();
+                    st.removeLast();
                  }
 
-                 ans.add(pq.peek()[0]);
+                 ans.add(st.peekLast()[0]);
 
                  i++;
              }
 
         }
-
-
-
-
-        
-
-           
-
 
           int [] ans1 = new int [ans.size()];
 
