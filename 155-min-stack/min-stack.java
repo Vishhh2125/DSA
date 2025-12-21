d9e07d9e6354cb94d7efc22002d22007e967a819
Vishnu Uplenchwar <167734203+Vishhh2125@users.noreply.github.com>
2025-12-22 01:57:33 +0530

    Time: 5 ms (77.39%) | Memory: 47.1 MB (19.70%) - LeetSync

--- 155-min-stack/min-stack.java
+++ 155-min-stack/min-stack.java
@@ -1,44 +1,61 @@
-import java.util.Stack;
-
 class MinStack {
-    private Stack<Long> stack;
-    private long min;
+
+      Deque<Pair> st ;
+      
+     
+     class Pair{
+
+        int val;
+        int min;
+
+
+        Pair(int val,int min){
+            this.val=val;
+            this.min=min;
+        }
+     }
 
     public MinStack() {
-        stack = new Stack<>();
-    }
 
+
+
+    st= new ArrayDeque<>();
+        
+    }
+    
     public void push(int val) {
-        if (stack.isEmpty()) {
-            min = val;
-            stack.push((long) val);
-        } else if (val >= min) {
-            stack.push((long) val);
-        } else {
-            // store encoded value
-            stack.push(2L * val - min);
-            min = val;
-        }
-    }
 
+       int  min= st.isEmpty() ? val : Math.min(st.peek().min,val);
+
+        st.push(new Pair(val,min));
+        
+    }
+    
     public void pop() {
-        long top = stack.pop();
-        if (top < min) {
-            // decode previous min
-            min = 2L * min - top;
-        }
-    }
 
+        st.pop();
+        
+    }
+    
     public int top() {
-        long top = stack.peek();
-        if (top >= min) {
-            return (int) top;
-        } else {
-            return (int) min;
-        }
-    }
 
+       return st.peek().val;
+        
+    }
+    
     public int getMin() {
-        return (int) min;
+
+
+        return st.peek().min;
+        
     }
 }
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
