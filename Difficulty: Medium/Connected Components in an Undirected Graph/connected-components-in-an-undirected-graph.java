217abef50b3244f4cd86e9842ed46cce7196b7c6
Vishnu Uplenchwar <167734203+Vishhh2125@users.noreply.github.com>
2025-12-29 00:17:57 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Connected Components in an Undirected Graph/connected-components-in-an-undirected-graph.java
+++ Difficulty: Medium/Connected Components in an Undirected Graph/connected-components-in-an-undirected-graph.java
@@ -2,73 +2,64 @@ class Solution {
     public ArrayList<ArrayList<Integer>> getComponents(int V, int[][] edges) {
         // code here
         
-        int n = edges.length;
+        boolean [] visited= new boolean[V];
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
         
-        
-        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
-        
-        boolean[] visited = new boolean[V];
+        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
         for(int i=0;i<V;i++){
             
-            list.add(new ArrayList<>());
+            adj.add(new ArrayList<>());
+            
         }
         
         
         
         for(int i=0;i<edges.length;i++){
             
-            list.get(edges[i][0]).add(edges[i][1]);
-             list.get(edges[i][1]).add(edges[i][0]);
+            adj.get(edges[i][0]).add(edges[i][1]);
+            adj.get(edges[i][1]).add(edges[i][0]);
             
         }
         
-        
-        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
         
         
         for(int i=0;i<V;i++){
             
+            ArrayList<Integer> ans = new ArrayList<>();
             if(!visited[i]){
-                // teh starting node is not visited lest star te dfs from here e
+            dfs(i,visited,adj,ans);
                 
-                 
-                   ArrayList<Integer> ans = new ArrayList<>();
-                    Deque<Integer>  q = new ArrayDeque<>();
-                    q.offer(i);
-                    visited[i]=true;
-                    
-     
-                    while(!q.isEmpty() ){
-                        
-                        
-                        int start=q.poll();
-                        ans.add(start);
-                        
-                        
-                        for(Integer node :list.get(start)){
-                            
-                            if(!visited[node]){
-                                visited[node]=true;
-                                q.offer(node);
-                                
-                            }
-                            
-                        }
-                        
-                        
-                        
-                        
-                    }
-                    
-                 result.add(ans);   
-              
-             
             }
-             
             
+            if(ans.size()>0){
+                
+                result.add(ans);
+                
+            }
         }
         
         
-        return result ;
+        return result;
+        
+        
+    }
+    
+    
+    
+    public void dfs(int node ,boolean []visited,ArrayList<ArrayList<Integer>> adj,ArrayList<Integer>ans){
+        
+        
+        
+        visited[node]=true;
+        ans.add(node);
+        
+        
+        for(int nbr :adj.get(node)){
+            
+            if(!visited[nbr]){
+                
+                dfs(nbr,visited,adj,ans);
+            }
+        }
     }
 }
