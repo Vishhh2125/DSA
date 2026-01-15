cc79dabf3e2dca42f0faded2eb977b389c6cd49f
Vishnu Uplenchwar <167734203+Vishhh2125@users.noreply.github.com>
2026-01-15 11:50:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Tree Boundary Traversal/tree-boundary-traversal.java
+++ Difficulty: Medium/Tree Boundary Traversal/tree-boundary-traversal.java
@@ -12,81 +12,78 @@ class Node {
 
 class Solution {
     
-   public static  void left_boundary(Node current,ArrayList<Integer> ans){
+    
+    void left_boundary(Node current ,ArrayList<Integer> ans ){
+     if(current==null) return ;
+
+        if(current.left==null && current.right==null)  return ; //bcz leaf we only eed left boundaryb
         
-        if(current==null)  return ;
         
-          if (current.left != null) {
-            ans.add(current.data); // not leaf
-            left_boundary(current.left, ans);
-        } else if (current.right != null) {
-            ans.add(current.data); // not leaf
-            left_boundary(current.right, ans);
+        if(current.left!=null){
+            ans.add(current.data);
+            left_boundary(current.left,ans);
+        }else if(current.right!=null){
+            ans.add(current.data);
+            left_boundary(current.right,ans);
         }
+        
+        
     }
     
     
-     public static void right_boundary(Node current,ArrayList<Integer> ans){
-         if(current==null)  return ;
-         
-         
+    void leaf (Node current ,ArrayList<Integer>ans){
         
-         
-                        if (current.right != null) {
-                    right_boundary(current.right, ans);
-                    ans.add(current.data);  // add after recursion for reverse order
-                } else if (current.left != null) {
-                    right_boundary(current.left, ans);
-                    ans.add(current.data);
-                }
-
-        
+        if(current==null) return ;
          
          
-     }
-     
-     
-     public static void leaf(Node current,ArrayList<Integer> ans ){
-         
-         if(current==null)  return ;
-         
-         
-         if(current.right==null && current.left==null){
-             
-             ans.add(current.data);
-             //this is leaf node 
-          
-             
-             
-         }
-         
-         leaf(current.left,ans);
-         leaf(current.right,ans);
-         
-         
-     }
-    ArrayList<Integer> boundaryTraversal(Node root) {
-        // code here
-        
-        
-        ArrayList<Integer> ans = new ArrayList<>();
-        
-        
-        if(root==null)  return ans;
-        
-        if(root.right!=null || root.left!=null){
+         //similar to preorder 
+        if(current.left==null && current.right==null){
+            ans.add(current.data);
+        }else{
             
-         ans.add(root.data); //add root it is not leaft  
+        leaf(current.left,ans);
+        leaf(current.right,ans);
+        
+        }
+    }
+    
+    void right_boundary(Node current,ArrayList<Integer> ans ){
+        
+        if(current==null) return ;
+        
+        if(current.left==null && current.right==null) return ;
+        
+        
+        if(current.right!=null){
+            right_boundary(current.right,ans);
+            
+        }else if(current.left!=null){
+            right_boundary(current.left,ans);
         }
         
         
-        left_boundary(root.left,ans); //left  boundary 
+          ans.add(current.data); //comes in reverse order;
+    }
+    
+    
+    ArrayList<Integer> boundaryTraversal(Node root) {
+        // code here
+        if(root==null) return new ArrayList<>();
+        ArrayList<Integer>  ans = new ArrayList<>();
+        
+        if(root.left==null && root.right==null){
+            ans.add(root.data);
+            return ans ;
+            //only one root node 
+        }
+        
+         ans.add(root.data); //add root first 
+        left_boundary(root.left,ans);
         leaf(root,ans);
-        right_boundary(root.right,ans);//right boundary 
+        right_boundary(root.right,ans);
         
+        return ans ;
         
-        
-        return ans;
         
     }
 }
