118b7641e42d79c8b4f0d306248508289961a23e
Vishnu Uplenchwar <167734203+Vishhh2125@users.noreply.github.com>
2026-01-20 06:12:44 +0530

    Time: 4 ms (70.40%) | Memory: 44.5 MB (63.88%) - LeetSync

--- 257-binary-tree-paths/binary-tree-paths.java
+++ 257-binary-tree-paths/binary-tree-paths.java
@@ -15,55 +15,50 @@
  */
 class Solution {
 
-    public static void solution(TreeNode current, StringBuilder output,List<String > ans ){
+    public void solution(TreeNode current,List<String > ans ,StringBuilder output ){
+         if(current==null )  return ;
 
-        if(current==null)  return ;
 
-        //stoore sie to recurs the ans 
-        int size= output.length();
-
-        //add to the output 
-         if(output.length()==0){
-                  output.append(current.val); //add ans 
-
+        //add into output 
+        if(output.length()==0){
+            output.append(current.val+"");
         }else{
+            
+            output.append("->"+current.val+"");
 
-                output.append("->"+current.val); //add ans 
         }
 
+        int size=output.length();
 
-        
-         // if leaf node add ans 
+        //search for left 
+        solution(current.left,ans,output);
+        //backtrack
+        String str1=output.substring(0,size);
+        output= new StringBuilder(str1);
+        //search for right
+        solution(current.right,ans,output);
+        //backtrack 
+        String str2=output.substring(0,size);
+        output= new StringBuilder(str2);
+
         if(current.left==null && current.right==null){
-            ans.add(new String(output.toString()));//add to ans 
-             output.setLength(size); //backtrack
-
-            return ;
+            ans.add(new String (output.toString()));
             
         }
-        
-       
-
-        
-            //go left 
-            solution(current.left,output,ans);//recurse
-             // go right 
-            solution(current.right,output,ans); ///recurse
-          output.setLength(size); //backtrack only one time beacuse we need teh same input in both th funtion 
 
 
-        
 
 
     }
     public List<String> binaryTreePaths(TreeNode root) {
 
-        List<String> ans = new ArrayList<>();
+        List<String> ans= new ArrayList<>();
 
-        solution(root,new StringBuilder(),ans);
+        solution(root,ans,new StringBuilder());
+
 
         return ans ;
-        
+
         
     }
 }
