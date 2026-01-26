3d1a0c3975808c0c8e3066b44fdb0ad9f8d4d354
Vishnu Uplenchwar <167734203+Vishhh2125@users.noreply.github.com>
2026-01-26 15:43:08 +0530

    Time: 2 ms (10.91%) | Memory: 43.4 MB (12.31%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        s=s.trim();
+       int count=0;
+
+        for(int i=0;i<s.length();i++){
+
+            if(s.charAt(i)==' '){
+                count=0;
+
+                while(i<s.length() && s.charAt(i)==' ') i++;
+            }
+
+             count++;
+            
+
+
+        }
+
+        return count;
+        
+    }
+}
