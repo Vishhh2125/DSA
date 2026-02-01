e7688af4e0c386ff6d2dc63ffb5ef5985dd7b471
Vishnu Uplenchwar <167734203+Vishhh2125@users.noreply.github.com>
2026-02-01 16:03:32 +0530

    Time: 24 ms (55.30%) | Memory: 76.3 MB (76.51%) - LeetSync

---
+++ 219-contains-duplicate-ii/contains-duplicate-ii.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+
+
+    HashSet<Integer> set = new HashSet<>();
+
+       int i=0;
+    for(int j=0;j<nums.length;j++){
+        if(set.contains(nums[j])) return true;
+        set.add(nums[j]);
+
+
+        if(j-i>=k){
+            set.remove(nums[i]);
+            i++;
+        }
+
+
+    }
+
+    return false;
+        
+    }
+}
