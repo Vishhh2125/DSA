e3e3ed22fb272128651f51924fcf90f5b427683c
Vishnu Uplenchwar <167734203+Vishhh2125@users.noreply.github.com>
2026-02-17 11:03:43 +0530

    Time: 1 ms (99.70%) | Memory: 43.9 MB (42.55%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -1,56 +1,54 @@
 class Solution {
     public int myAtoi(String s) {
 
-
-       int i=0;
-       int n =s.length();
-
-
-       while(i<n && s.charAt(i)==' ') i++;
-
-
-       int sign=1;
-
-
-       if(i<n && s.charAt(i)=='-') {
-       sign=-1;
-       i++;
-       }else if(i<n && s.charAt(i)=='+'){
-        //ignore as sign is positive 
-
-        i++;
-       }
-        
-
-
-       long  ans=0;
-       while(i<n  ){
-
-        if(s.charAt(i)>=48 && s.charAt(i)<=57){
-            // this is character just add number 
-
-            ans= ans*10 +( s.charAt(i)-'0');
+        s=s.trim();
+        int i=0;
+        int sign=1;
+         //check for sign if have 
+        if( i<s.length() && s.charAt(i)=='-'){
+            sign=-1;
             i++;
-        }else{
-            break;
-        }
-
-
-        if(sign*ans>Integer.MAX_VALUE){
-         return Integer.MAX_VALUE;
-        }
-        if(sign * ans<Integer.MIN_VALUE){
-          return Integer.MIN_VALUE;
-
-          //here while comapring why have we taken the -ans beacuse own ans is in positive form and sign is telling usweather is number neagtive or psoitive 
-
+        }else if(i<s.length() && s.charAt(i)=='+'){
+            sign=1;
+            i++;
 
         }
-    
-       }
+       
+       //skip teh leading zeros 
+
+       while( i<s.length() && s.charAt(i)=='0') i++;
+
+        
+         long ans =0;
 
 
-       return (int) ans * sign;
+        while(i<s.length() ){
+         
+         if(!Character.isDigit(s.charAt(i))) break ;
 
+
+          ans=ans*10 + (s.charAt(i)-'0');
+          i++;
+
+
+          if( sign==1 && ans>Integer.MAX_VALUE) {
+            return Integer.MAX_VALUE;
+
+          }
+
+          if(sign ==-1 && -ans<Integer.MIN_VALUE){
+            return Integer.MIN_VALUE;
+          }
+
+       
+
+
+        }
+
+      
+
+
+        return (int) ( sign * ans) ;
+        
     }
 }
