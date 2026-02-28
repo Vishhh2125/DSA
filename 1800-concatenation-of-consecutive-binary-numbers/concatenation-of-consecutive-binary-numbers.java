766e5885c9810c4439cb961d4a896e48a4d51464
Vishnu Uplenchwar <167734203+Vishhh2125@users.noreply.github.com>
2026-02-28 17:45:35 +0530

    Time: 1576 ms (5.81%) | Memory: 112 MB (6.98%) - LeetSync

--- 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.java
+++ 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.java
@@ -1,57 +1,50 @@
 class Solution {
 
-    public void tobinary(StringBuilder str ,int n ){
+    public void tobinary(int n ,StringBuilder str ){
+      
 
-          StringBuilder temp = new StringBuilder();
-
+      StringBuilder temp = new StringBuilder();
+    
         while(n>0){
 
-            temp.append(n%2);
-            n=n/2;
+           char ch = (n & 1)==0 ? '0' :'1';
+
+           temp.append(ch);
+           n=n>>1;
+
         }
+
 
         str.append(temp.reverse());
     }
 
-    public long todecimal(StringBuilder str ){
+
+    public long todecimal(StringBuilder str){
+
         long ans =0;
-        int mod = 1000000007;
-          int n=str.length();
+        long mod= 1000000007;
 
-
-          for(int i=0;i<n;i++){
-
-                    ans = ((ans << 1) % mod);   // shift safely with mod
-
-
+       
+        for(int i=0;i<str.length();i++){
+            ans=(ans<<1) %mod; //lleft shift 
             if(str.charAt(i)=='1'){
+                ans= (ans+1)%mod; //plus one at teh end of teh oth index of number 
 
-               ans=(ans+1) %mod;
             }
+        }
 
-           
-          }
-
-          return ans ;
-
-      
+        return ans ;
     }
     public int concatenatedBinary(int n) {
-
 
         StringBuilder str= new StringBuilder();
 
 
-        for(int i=1;i<=n;i++){
-        tobinary(str,i);
-
+        for(int i=1;i<=n; i++){
+            tobinary(i,str);
         }
 
-
-        return (int ) todecimal(str);
-
-
-          
+        return (int) todecimal(str);
         
     }
 }
