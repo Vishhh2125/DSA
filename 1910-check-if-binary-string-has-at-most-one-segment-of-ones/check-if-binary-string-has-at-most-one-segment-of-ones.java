a9950727d71ccdf49e7e51fdd71b04cc42de6404
Vishnu Uplenchwar <167734203+Vishhh2125@users.noreply.github.com>
2026-03-06 08:36:33 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (91.51%) - LeetSync

--- 1910-check-if-binary-string-has-at-most-one-segment-of-ones/check-if-binary-string-has-at-most-one-segment-of-ones.java
+++ 1910-check-if-binary-string-has-at-most-one-segment-of-ones/check-if-binary-string-has-at-most-one-segment-of-ones.java
@@ -1,23 +1,26 @@
 class Solution {
     public boolean checkOnesSegment(String s) {
+
+
+        // there is ony one segment of 1 we can use slifing wndow ot count segement 
        
-       boolean flag =false;
+       int count =0;
+        
+        
        for(int i=0;i<s.length();i++){
-        if(s.charAt(i)=='1' && flag==false){
-            flag=true;
+
+        if(s.charAt(i)=='1'){
+            count++;
 
             while(i<s.length() && s.charAt(i)=='1') i++;
 
-        }else{
-
-         if(flag==true && s.charAt(i)=='1') return false;
         }
-         
 
+        
 
-          
        }
 
-       return true ;
+       if(count ==1) return true ;
+       else return false;
     }
 }
