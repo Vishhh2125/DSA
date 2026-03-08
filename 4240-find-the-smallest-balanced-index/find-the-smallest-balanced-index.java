5544683a349053141a3caf4ea2a7553250a44e38
Vishnu Uplenchwar <167734203+Vishhh2125@users.noreply.github.com>
2026-03-08 12:42:43 +0530

    Time: 10 ms (24.77%) | Memory: 147.1 MB (5.53%) - LeetSync

--- 4240-find-the-smallest-balanced-index/find-the-smallest-balanced-index.java
+++ 4240-find-the-smallest-balanced-index/find-the-smallest-balanced-index.java
@@ -1,19 +1,34 @@
 class Solution {
     public int smallestBalancedIndex(int[] nums) {
+
         int n = nums.length;
         long[] leftSum = new long[n];
-        for(int i = 1; i < n; i++) leftSum[i] = leftSum[i - 1] + nums[i - 1];
+        for (int i = 1; i < n; i++) {
+            leftSum[i] = leftSum[i - 1] + nums[i - 1];
+        }
 
-        long pro = 1;
-        int ans = -1;
+        long rightProduct = 1;
+        int answer = -1;
 
-        for(int i = n - 1; i >= 0; i--) {
-            if(leftSum[i] == pro) ans = i;
-            if(i > 0) {
-                if(pro > 1e14 / nums[i]) pro = (long)1e18;
-                else pro *= nums[i];
+        for (int i = n - 1; i >= 0; i--) {
+
+          
+            if (leftSum[i] == rightProduct) {
+                answer = i;
+            }
+
+            if (i > 0) {
+
+                
+                if (rightProduct > Long.MAX_VALUE / nums[i]) {
+                    rightProduct = Long.MAX_VALUE; 
+                } 
+                else {
+                    rightProduct = rightProduct * nums[i];
+                }
             }
         }
-        return ans;
+
+        return answer;
     }
 }
