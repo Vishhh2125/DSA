a6b6e5b822eb2cad80396e2c18694d3b124d9727
Vishnu Uplenchwar <167734203+Vishhh2125@users.noreply.github.com>
2026-06-06 13:17:11 +0530

    Time: 14 ms (37.22%) | Memory: 48.7 MB (87.25%) - LeetSync

--- 744-network-delay-time/network-delay-time.java
+++ 744-network-delay-time/network-delay-time.java
@@ -1,82 +1,88 @@
 class Solution {
-    public class Pair{
-        int vertex;
-        int weight ;
 
-        Pair(int vertex,int weight ){
-            this.weight=weight;
-            this.vertex=vertex;
+    public void dijkstra( ArrayList<ArrayList<int[]>>  adj, int [] ans ,PriorityQueue<int []>  q,int src){
+
+        ans[src]=0;
+
+        q.offer(new int []{src,0});
+
+
+        while(!q.isEmpty()){
+
+            int [] arr =  q.poll();
+            int u =arr[0];
+            int w=arr[1];
+
+
+            if(w>ans[u])  continue;
+
+
+            for(int [] nbr :adj.get(u)){
+
+                int new_weight=w+nbr[1];
+
+                if(new_weight<ans[nbr[0]]){
+                    //we got another low weight path 
+
+                    ans[nbr[0]]=new_weight;
+
+                    q.offer(new int []{nbr[0],new_weight});
+                }
+            }
         }
-    }
-    public int networkDelayTime(int[][] edges, int V, int src) {
-
-         
-        ArrayList<ArrayList<Pair>> adj = new ArrayList<>();
-       
-       for(int i=0;i<V+1;i++){
-           adj.add(new ArrayList<>());
-           
-       }
-       
-       
-       for(int i=0;i<edges.length;i++){
-           
-           int u=edges[i][0];
-           int v = edges[i][1];
-           int weight=edges[i][2];
-           
-           adj.get(u).add(new Pair(v,weight)); //directed grpah only            
-       }
-     
-
-        int [] ans = new int [V+1];
-       Arrays.fill(ans, Integer.MAX_VALUE); //all other vertexx and infinte 
-       ans[src]=0; //shorted distance for sorce is zerof rom start 
-
-        PriorityQueue<Pair> pq = new PriorityQueue<>(
-        (a, b) -> a.weight - b.weight
-    );
-    
-    //insert teh src
-    pq.offer(new Pair(src, 0));
 
 
-    while(!pq.isEmpty()){
-         
-         Pair vertex= pq.poll();
-         int weight= vertex.weight;
-         
-        if(weight > ans[vertex.vertex]) continue;   // 🔥 replaces visited[]
-
-        
-         //go to all nbr 
-         for(Pair nbr :adj.get(vertex.vertex)){
-             
-             int nextweight=weight+nbr.weight;
-             
-             if(nextweight <ans[nbr.vertex]){
-                 //the next weoght path is smaller than te alredy exisiting weight in ans 
-                 ans[nbr.vertex]=nextweight;
-             pq.offer(new Pair(nbr.vertex,nextweight));
-                 
-             }
-             
-             
-         }
-     }
-
-     int max=0;
-
-    for(int i = 1; i <= V; i++) {
-    if(ans[i] == Integer.MAX_VALUE) return -1;  // unreachable node
-    max = Math.max(max, ans[i]);
-  }
-
-
-     return max;
         
     }
 
+    public int networkDelayTime(int[][] times, int n, int k) {
+        
+
+        ArrayList<ArrayList<int[]>>  adj = new ArrayList<>();
+
+        for(int i =0;i<=n;i++){
+            adj.add(new ArrayList<>());
+        }
+
+
+        for(int i =0;i<times.length;i++){
+
+            int u =times[i][0];
+            int v =times[i][1];
+            int w =times[i][2];
+
+            adj.get(u).add(new int []{v,w});
+
+        }
+
+        int [] ans = new int [n+1];
+       PriorityQueue<int []>  q= new PriorityQueue<>((a,b)->{
+            return a[1]-b[1];
+        });
+         Arrays.fill(ans, Integer.MAX_VALUE);
+         
+       dijkstra(adj,ans,q,k);
+
+
+
+
+       int max=0;
+
+       for(int i =1;i<ans.length;i++){
+        int num =ans[i];
+        if(num==Integer.MAX_VALUE) return -1 ;
+
+
+        max=Math.max(max,num);
+       }
+
+
+
+       return max;
+
+       //bfs traversal 
 
      
+
+    }
 }
