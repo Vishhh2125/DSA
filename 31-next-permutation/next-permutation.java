16ceb3885b630e096d68e8032a7c0c780ac7800d
Vishnu Uplenchwar <vishnuuplenchwar.professional@gmail.com>
2026-08-03 16:19:35 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (52.43%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,60 +1,47 @@
 class Solution {
 
-    public void reverse(int [] arr,int i ,int j ){
+    public void rotate(int [] nums,int i ,int j ){
 
         while(i<j){
-            int temp =arr[i];
-            arr[i]=arr[j];
-            arr[j]=temp;
+            int temp =nums[i];
+            nums[i]=nums[j];
+            nums[j]= temp  ;
             i++;
             j--;
         }
     }
-    public void nextPermutation(int[] arr) {
-
-            int n =arr.length;
-        int pivot_index=-1;
-
-
-        for(int i=n-2;i>=0;i--){
-
-            if(arr[i]<arr[i+1]) {
-                pivot_index=i;
-                break;
-            }
-
-
-        }
-
-
-        if(pivot_index==-1){
-            //means no pivot so rever full arr 
-            reverse(arr,0,n-1);
-            return ;
-        }
-
-
-         int i=n-1;
-        for(;i>=pivot_index;i--){
-
-            if(arr[i]>arr[pivot_index]) break;
-        }
-
-
-        int temp=arr[i];
-        arr[i]=arr[pivot_index];
-        arr[pivot_index]=temp;
-
-
-        reverse(arr,pivot_index+1,n-1);
-
-
-
-
-
-
-
-
+    public void nextPermutation(int[] nums) {
         
+      int k = nums.length-2 ;
+
+
+      while(k>=0 && nums[k]>=nums[k+1]){
+        k--;
+      }
+
+      if(k<0){
+        rotate(nums,0, nums.length-1);
+        //beacuse the currnet is teh biggest element form rearrnging the number itself to we need the series bemoes the smallest which is by rveersing all 
+      }else{
+
+
+        int j =nums.length-1;
+        while(nums[k]>=nums[j]){
+            j--;
+        }
+        int temp = nums[k];
+        nums[k]=nums[j];
+        nums[j]=temp;
+
+        rotate(nums,k+1,nums.length-1);
+
+      }
+
+
+
+
+      
+ 
+    
     }
 }
