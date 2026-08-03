7ac86546ac22b0a61e23e7bf5fab52ba474cbb13
Vishnu Uplenchwar <vishnuuplenchwar.professional@gmail.com>
2026-08-03 15:51:09 +0530

    Time: 3 ms (100.00%) | Memory: 175.8 MB (15.59%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
@@ -1,34 +1,28 @@
 class Solution {
-
-    public void swap ( int[]arr ,int i ,int j ){
-        int temp =arr[i];
-        arr[i]=arr[j];
-        arr[j]=temp;
-    }
-    public int[] rearrangeArray(int[] arr) {
-
-        int i=0;
-        int j =1;
-
-        int c=0;
-        int [] ans= new int[arr.length];
-
-        while(c<arr.length){
-
-            if(arr[c]>0){
-                //swap with positive
-               ans[i]=arr[c];
-                i=i+2;
+    public int[] rearrangeArray(int[] nums) {
 
 
-            }else{
-               ans[j]=arr[c]; 
-                j=j+2;
-            }
-            c++;
+
+
+        int [] ans = new int [nums.length];
+        int p=0;
+        int n =1;
+        for(int i =0;i<nums.length;i++){
+           if(nums[i]<0){
+            ans[n]=nums[i];
+            n+=2;
+
+           }else{
+
+            ans[p]=nums[i];
+            p+=2;
+
+           }
+
         }
 
-        return ans;
+
+        return ans ;
         
     }
 }
