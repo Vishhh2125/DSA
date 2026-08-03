37c57370e909f8034567a0a4a5c838fc9f70bebf
Vishnu Uplenchwar <vishnuuplenchwar.professional@gmail.com>
2026-08-03 18:56:32 +0530

    Time: 23 ms (93.60%) | Memory: 48.9 MB (34.20%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -3,24 +3,31 @@ class Solution {
 
         HashMap<Integer,Integer> map = new HashMap<>();
 
-        map.put(0,1);
-        int sum=0;
-        int count=0;
+        map.put(0,1);   //zero sum subaarya we have 1 intiila case
+        
+         int ans =0;
+         int sum =0;
+        for(int j =0;j<nums.length;j++){
+          
+          sum+=nums[j];
 
-        for(int i=0;i<nums.length;i++){
-            sum+=nums[i]; // get current sum
+          //check for conatine how many num[i]=num[j](sum)-k
 
-            if(map.containsKey(sum-k)){
-                count+=map.get(sum-k);
-            }
-            map.put(sum,map.getOrDefault(sum,0)+1); //update teh sum count 
+          if(map.containsKey(sum-k)){
 
+             ans+=map.get(sum-k);
+          }
+
+          //add the currnets um in ahshmap 
+
+          map.put(sum,map.getOrDefault(sum,0)+1);
 
         }
 
-        return count;
 
 
+        return ans ;
+        
         
     }
 }
